da1182e5f587beb40eb025818b40a0c91eda7ee5
prothegee <prothegee@outlook.com>
2022-08-22 09:11:26 +0700

    checkup thumbail and network test

--- Code/tools/NetworkRequest.h
+++ Code/tools/NetworkRequest.h
@@ -2,12 +2,26 @@
 #include "ceblanktc1.h"
 
 
+// #include <../../CryPlugins/CryHTTP/Interface/IHttpPlugin.h> // from Code/CryPlugins/CryHTTP/Interface/IHttpPlugin.h
+// #include <../../CryPlugins/CryHTTP/Module/HttpRequestImpl_cURL.h> // from Code/CryPlugins/CryHTTP/Module/HttpRequestImpl_cURL.h
+
+
+#include <cpr/cpr.h>
+
+
 namespace ceblanktc1
 {
 namespace tools
 {
 
 
+/**
+ * todo: check how CryHttp works
+ * for GET/POST/PUT/DELETE & etc
+ * 
+ * but not websocket.
+ * 
+ */
 class NetworkRequest
 {
 public:
@@ -17,22 +31,16 @@ public:
     NetworkRequest(/* args */);
     virtual ~NetworkRequest();
 
-
-protected:
-    auto TBD1(string url)
+public:
+    static std::string TBD2()
     {
-        gEnv->pSystem->GetIPluginManager()->QueryPlugin<Cry::Http::IPlugin>();
-        //                                                   ^~~~ you what? Compiller not sayin any err.
+        std::string url = "https://raw.githubusercontent.com/prothegee/prothegee/main/data/users.json";
+
+        cpr::Response r = cpr::Get(cpr::Url{url});
+
+        return r.text;
     }
 };
-
-
-NetworkRequest::NetworkRequest(/* args */)
-{
-}
-NetworkRequest::~NetworkRequest()
-{
-}
 
 
 
